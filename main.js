195868ea3eda59d369c136a12e67b6e601953436
AliMoradpour <Mr.mrdp@yahoo.com>
2022-10-17 11:58:38 -0700

    complete the project

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $(".text").textillate({
+    loop: true,
+    in: {
+      effect: "fadeInDownBig",
+      delayScale: 3,
+      delay: 20,
+      //   shuffle: true,
+    },
+    out: {
+      effect: "bounceOut",
+      delayScale: 2,
+      delay: 20,
+      reverse: true,
+    },
+  });
+});
